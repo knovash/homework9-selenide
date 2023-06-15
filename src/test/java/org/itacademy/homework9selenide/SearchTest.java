afa08b3160c03f0a29e97b5becce9754f9747764
Konstantin <novash.ki@gmail.com>
2023-06-15 09:12:54 +0300

    screenshots was added

--- src/test/java/org/itacademy/homework9selenide/SearchTest.java
+++ src/test/java/org/itacademy/homework9selenide/SearchTest.java
@@ -1,19 +1,24 @@
 package org.itacademy.homework9selenide;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.Description;
 import io.qameta.allure.Issue;
 import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.Level;
 import org.itacademy.homework9selenide.models.MenuItem;
 import org.itacademy.homework9selenide.steps.SearchSteps;
 import org.itacademy.homework9selenide.utils.Config;
 import org.itacademy.homework9selenide.utils.DataProviderSearch;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
+import com.codeborne.selenide.testng.ScreenShooter;
 
 import static com.codeborne.selenide.Selenide.open;
 
 @Log4j2
+@Listeners
 public class SearchTest extends BaseTest {
 
     private SearchSteps searchSteps;
@@ -23,8 +28,8 @@ public class SearchTest extends BaseTest {
         log.info("BEFORE METHOD get page " + Config.getPageSearch());
         open(Config.getPageSearch());
         searchSteps = new SearchSteps();
-        Configuration.pageLoadTimeout = 300;
-        Configuration.timeout = 300;
+        Configuration.pageLoadTimeout = 30000;
+        Configuration.timeout = 30000;
     }
 
     @Description("Verifys search result items")
