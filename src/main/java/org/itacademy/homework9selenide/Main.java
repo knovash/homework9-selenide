e1845e9017d957f61e0f6c07f6052ccde69beab2
Konstantin <novash.ki@gmail.com>
2023-06-20 14:44:57 +0300

    cheapest product

--- src/main/java/org/itacademy/homework9selenide/Main.java
+++ src/main/java/org/itacademy/homework9selenide/Main.java
@@ -1,10 +1,7 @@
 package org.itacademy.homework9selenide;
 
-import org.itacademy.homework9selenide.utils.MenuUtil;
-
 public class Main {
 
     public static void main(String[] args) {
-        MenuUtil.createJsonFile();
     }
 }
